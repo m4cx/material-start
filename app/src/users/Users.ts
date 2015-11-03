e375c9b49300f2bf6b1b1506318803b38f2612bf
M4cX <macgyverx83@gmail.com>
2015-11-03 21:57:47 +0100

    - 	Rewrite module definition to TypeScript
    - 	Initial Build-Chain through gulp

---
+++ app/src/users/Users.ts
@@ -0,0 +1,9 @@
+/// <reference path="../../../typings/tsd.d.ts" />
+
+module Users {
+  'use strict';
+  
+  // Prepare the 'users' module for subsequent registration of controllers and delegates
+  angular.module('users', [ 'ngMaterial' ]);
+  
+}
---
+++ gulpfile.js
@@ -0,0 +1,11 @@
+var gulp = require("gulp"),
+	typescript = require("gulp-typescript");
+	
+gulp.task("build:typescript", function() {
+	gulp.src(["app/src/users/users.ts"])
+	.pipe(typescript({
+		target: "ES5",
+		
+	}))
+	.pipe(gulp.dest("app/src/users"));
+});
